58e05dcc2822385eb4f5befd2617fc2e745808a4
kcfuler <2842961263@qq.com>
2022-10-11 09:52:53 +0800

    fix: refactor and achieve login

---
+++ src/server.js
@@ -0,0 +1,92 @@
+const http = require('http');
+const path = require('path');
+const { SDK } = require('casdoor-nodejs-sdk');
+const fs = require('fs');
+const fetch = require("node-fetch")
+
+//init sdk
+const cert = `
+-----BEGIN CERTIFICATE-----
+MIIE+TCCAuGgAwIBAgIDAeJAMA0GCSqGSIb3DQEBCwUAMDYxHTAbBgNVBAoTFENh
+c2Rvb3IgT3JnYW5pemF0aW9uMRUwEwYDVQQDEwxDYXNkb29yIENlcnQwHhcNMjEx
+MDE1MDgxMTUyWhcNNDExMDE1MDgxMTUyWjA2MR0wGwYDVQQKExRDYXNkb29yIE9y
+Z2FuaXphdGlvbjEVMBMGA1UEAxMMQ2FzZG9vciBDZXJ0MIICIjANBgkqhkiG9w0B
+AQEFAAOCAg8AMIICCgKCAgEAsInpb5E1/ym0f1RfSDSSE8IR7y+lw+RJjI74e5ej
+rq4b8zMYk7HeHCyZr/hmNEwEVXnhXu1P0mBeQ5ypp/QGo8vgEmjAETNmzkI1NjOQ
+CjCYwUrasO/f/MnI1C0j13vx6mV1kHZjSrKsMhYY1vaxTEP3+VB8Hjg3MHFWrb07
+uvFMCJe5W8+0rKErZCKTR8+9VB3janeBz//zQePFVh79bFZate/hLirPK0Go9P1g
+OvwIoC1A3sarHTP4Qm/LQRt0rHqZFybdySpyWAQvhNaDFE7mTstRSBb/wUjNCUBD
+PTSLVjC04WllSf6Nkfx0Z7KvmbPstSj+btvcqsvRAGtvdsB9h62Kptjs1Yn7GAuo
+I3qt/4zoKbiURYxkQJXIvwCQsEftUuk5ew5zuPSlDRLoLByQTLbx0JqLAFNfW3g/
+pzSDjgd/60d6HTmvbZni4SmjdyFhXCDb1Kn7N+xTojnfaNkwep2REV+RMc0fx4Gu
+hRsnLsmkmUDeyIZ9aBL9oj11YEQfM2JZEq+RVtUx+wB4y8K/tD1bcY+IfnG5rBpw
+IDpS262boq4SRSvb3Z7bB0w4ZxvOfJ/1VLoRftjPbLIf0bhfr/AeZMHpIKOXvfz4
+yE+hqzi68wdF0VR9xYc/RbSAf7323OsjYnjjEgInUtRohnRgCpjIk/Mt2Kt84Kb0
+wn8CAwEAAaMQMA4wDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQsFAAOCAgEAn2lf
+DKkLX+F1vKRO/5gJ+Plr8P5NKuQkmwH97b8CS2gS1phDyNgIc4/LSdzuf4Awe6ve
+C06lVdWSIis8UPUPdjmT2uMPSNjwLxG3QsrimMURNwFlLTfRem/heJe0Zgur9J1M
+8haawdSdJjH2RgmFoDeE2r8NVRfhbR8KnCO1ddTJKuS1N0/irHz21W4jt4rxzCvl
+2nR42Fybap3O/g2JXMhNNROwZmNjgpsF7XVENCSuFO1jTywLaqjuXCg54IL7XVLG
+omKNNNcc8h1FCeKj/nnbGMhodnFWKDTsJcbNmcOPNHo6ixzqMy/Hqc+mWYv7maAG
+Jtevs3qgMZ8F9Qzr3HpUc6R3ZYYWDY/xxPisuKftOPZgtH979XC4mdf0WPnOBLqL
+2DJ1zaBmjiGJolvb7XNVKcUfDXYw85ZTZQ5b9clI4e+6bmyWqQItlwt+Ati/uFEV
+XzCj70B4lALX6xau1kLEpV9O1GERizYRz5P9NJNA7KoO5AVMp9w0DQTkt+LbXnZE
+HHnWKy8xHQKZF9sR7YBPGLs/Ac6tviv5Ua15OgJ/8dLRZ/veyFfGo2yZsI+hKVU5
+nCCJHBcAyFnm1hdvdwEdH33jDBjNB6ciotJZrf/3VYaIWSalADosHAgMWfXuWP+h
+8XKXmzlxuHbTMQYtZPDgspS5aK+S4Q9wb8RRAYo=
+-----END CERTIFICATE-----
+`;
+
+const authCfg = {
+  endpoint: 'http://localhost:8000',
+  clientId: '06effe8f48f0047c7462',
+  clientSecret: '068e46ac783e773154c24fbc7f5e8bb69ff8b826',
+  certificate: cert,
+  orgName: 'built-in',
+}
+const sdk = new SDK(authCfg);
+
+const server = http.createServer((req,res)=>{
+  if(req.method === "GET"){
+    if(req.url === '/' || req.url[req.url.indexOf('/') + 1] === '?'){
+      fs.readFile(path.resolve(__dirname,'./web/index.html'),(err,data)=>{
+        res.setHeader("Content-Type","text/html");
+        res.write(data);
+        res.end();
+      });
+    }else if( req.url === "/api/userinfo"){// 多余
+      sdk.getUsers().then(result => {
+        let {data : users} = result;
+        let userinfo = [];
+        users.forEach((item)=>{
+          let temp = {};
+          temp.displayName = item.displayName || item.name;
+          temp.avatar = item.avatar;
+          temp.email = item.email || "not set";
+          userinfo.push(temp);
+        })
+
+        res.write(JSON.stringify(userinfo));
+        res.end();
+
+        }
+      )
+    }
+  }else if(req.method === "POST"){
+      let url = req.url;
+      let location1 = url.indexOf('code');
+      let location2 = url.indexOf('state');
+      const code = url.slice(location1+5,location2-1);
+      const state = url.slice(location2+6);
+      sdk.getAuthToken(code).then(result=> {
+        const user = sdk.parseJwtToken(result);
+        console.log(user);
+        res.write(JSON.stringify(user));
+        res.end();
+      });
+    }
+})
+
+server.listen('8080',()=>{
+  console.log('server listen at http://localhost:8080');
+})
